cb5a89747a8b4ec3f6b4c4478e2dbb00bd42b80b
Andrei Avram <andrei.avram@spyhce.com>
2020-12-25 12:07:47 +0200

    added created_at and message fields

--- gulguta/models.py
+++ gulguta/models.py
@@ -5,7 +5,8 @@ class Gulguta(models.Model):
     name = models.CharField(max_length=255)
     geo_long = models.FloatField()
     geo_lat = models.FloatField()
-
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to="gulgute")
 
     def __str__(self):
--- static/flmap.js
+++ static/flmap.js
@@ -1,6 +1,11 @@
-console.log("hello");
+let button = document.getElementById("addButton")
 
-var mymap = L.map('fl_map').setView([46.06549996715349, 23.570670843267617], 14);
+button.addEventListener("click", function (e) {
+    console.log("I was clicked");
+    window.location.href = "/addGulguta";
+})
+
+let mymap = L.map('fl_map').setView([46.06549996715349, 23.570670843267617], 14);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
